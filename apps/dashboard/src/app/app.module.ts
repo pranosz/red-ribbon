4dd09260793666370361c5ec3984da6339113ba1
Piotr Ranosz <pranosz@wp.pl>
2024-02-04 17:41:22 +0100

    Angular materials

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { SharedUiModule } from '@red-ribbon/shared/ui'
 import { UserService } from '@red-ribbon/shared/data-access-user';
+import {MatToolbarModule} from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     SharedUiModule,
+    MatToolbarModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [UserService],
